734bb7de27056f718885d981281f879cb254379a
jwdsy <fly20070315@163.com>
2017-09-13 14:20:36 +0800

    Dev (#2)
    * package add
    
    * package add
    
    * edit main
    
    * edit main

--- src/main/java/cn/jwdsy/Main.java
+++ src/main/java/cn/jwdsy/Main.java
@@ -6,5 +6,8 @@ package cn.jwdsy;
 public class Main {
 	public static void main(String[] args) {
 		System.err.println("Hello World");
+		for (int i = 0; i < 10; i++) {
+			System.err.println("i="+i);
+		}
 	}
 }
