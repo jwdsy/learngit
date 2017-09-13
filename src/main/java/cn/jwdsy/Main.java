99ec87e8181cb1fb0f2e2e0c3975f5d3fc6b881d
zhangfei5 <zhangfei5@jd.com>
2017-09-13 14:29:55 +0800

    edit Main Class file

--- src/main/java/cn/jwdsy/Main.java
+++ src/main/java/cn/jwdsy/Main.java
@@ -1,5 +1,10 @@
 package cn.jwdsy;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by zhangfei5 on 2017/9/13.
  */
@@ -8,6 +13,12 @@ public class Main {
 		System.err.println("Hello World");
 		for (int i = 0; i < 10; i++) {
 			System.err.println("i="+i);
+		}
+		Map<String, Object> iMap = new HashMap<>();
+		List<Integer> integers = Arrays.asList(new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
+		for (int i = 0; i < integers.size(); i++) {
+			Integer integer = integers.get(i);
+			iMap.put(integer.toString(), integer);
 		}
 	}
 }
