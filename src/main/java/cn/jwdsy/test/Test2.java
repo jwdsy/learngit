547d488e2dc90913f8696b51b9323f41ad3151da
byss <fly20070315@163.com>
2017-09-24 22:34:45 +0800

    edit again at dev

--- src/main/java/cn/jwdsy/test/Test2.java
+++ src/main/java/cn/jwdsy/test/Test2.java
@@ -7,5 +7,6 @@ public class Test2 {
 	public static void main(String[] args) {
 		System.err.println("Hello World");
 		System.err.println("edit at dev branch");
+		System.err.println("edit at dev branch again");
 	}
 }
