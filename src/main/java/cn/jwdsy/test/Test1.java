bea45c081b9ff0b15cbeaf91571b83875a77e9f2
byss <fly20070315@163.com>
2017-09-24 21:46:52 +0800

    edit on dev first

--- src/main/java/cn/jwdsy/test/Test1.java
+++ src/main/java/cn/jwdsy/test/Test1.java
@@ -8,5 +8,6 @@ public class Test1 {
 	public static void main(String[] args) {
 		System.err.println("Hello World");
 		System.err.println("Hello World1111111111111111");
+		System.err.println("edit on dev first");
 	}
 }
