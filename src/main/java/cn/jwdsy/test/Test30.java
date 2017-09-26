a75e46f465b4ad5bdca579025dae1b3f3f132e03
byss <fly20070315@163.com>
2017-09-26 23:53:06 +0800

    Test30 12345 Test40 12345

---
+++ src/main/java/cn/jwdsy/test/Test30.java
@@ -0,0 +1,14 @@
+package cn.jwdsy.test;
+
+/**
+ * Created by Admin on 2017/9/26.
+ */
+public class Test30 {
+	public static void main(String[] args) {
+		System.err.println("11111111111111");
+		System.err.println("22222222222222");
+		System.err.println("33333333333333");
+		System.err.println("444444444444444");
+		System.err.println("555555555555555555");
+	}
+}
---
+++ src/main/java/cn/jwdsy/test/Test40.java
@@ -0,0 +1,14 @@
+package cn.jwdsy.test;
+
+/**
+ * Created by Admin on 2017/9/26.
+ */
+public class Test40 {
+	public static void main(String[] args) {
+		System.err.println("11111111111111");
+		System.err.println("222222222222");
+		System.err.println("33333333333333");
+		System.err.println("4444444444444");
+		System.err.println("5555555555555555");
+	}
+}
