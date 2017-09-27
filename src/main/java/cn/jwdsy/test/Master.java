863ed07f4f686351d435e628c4d0500caa3d11be
zhangfei5 <zhangfei5@jd.com>
2017-09-27 13:52:39 +0800

    1

---
+++ src/main/java/cn/jwdsy/test/Master.java
@@ -0,0 +1,10 @@
+package cn.jwdsy.test;
+
+/**
+ * Created by zhangfei5 on 2017/9/27.
+ */
+public class Master {
+	public static void main(String[] args) {
+		System.err.println("1111111111");
+	}
+}
