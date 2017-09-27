81f6c99ea499155ad30d9ae1e526955abab2c766
zhangfei5 <zhangfei5@jd.com>
2017-09-27 11:53:09 +0800

    Master_2 1234

---
+++ src/main/java/cn/jwdsy/test/Master_2.java
@@ -0,0 +1,13 @@
+package cn.jwdsy.test;
+
+/**
+ * Created by zhangfei5 on 2017/9/27.
+ */
+public class Master_2 {
+	public static void main(String[] args) {
+		System.err.println("1111111111");
+		System.err.println("222222222");
+		System.err.println("333333333");
+		System.err.println("444444444");
+	}
+}
