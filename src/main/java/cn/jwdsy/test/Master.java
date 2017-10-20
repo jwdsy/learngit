dbb1e4510e026568da280c794e043870c5e878ca
zhangfei5 <zhangfei5@jd.com>
2017-10-20 14:40:37 +0800

    bigdecimal

--- src/main/java/cn/jwdsy/test/Master.java
+++ src/main/java/cn/jwdsy/test/Master.java
@@ -1,10 +1,20 @@
 package cn.jwdsy.test;
 
+import java.math.BigDecimal;
+
 /**
  * Created by zhangfei5 on 2017/9/27.
  */
 public class Master {
 	public static void main(String[] args) {
 		System.err.println("1111111111");
+		BigDecimal b1 = new BigDecimal(1);
+		BigDecimal b2 = null;
+		try {
+			b1.multiply(b2);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 }
