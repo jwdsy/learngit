6a68e4567c8659d455b597880b14358d646c8dd5
zhangfei5 <zhangfei5@jd.com>
2017-10-20 14:41:43 +0800

    base on mas

---
+++ src/main/java/cn/jwdsy/test/BaseOnMaster.java
@@ -0,0 +1,10 @@
+package cn.jwdsy.test;
+
+/**
+ * Created by zhangfei5 on 2017/10/20.
+ */
+public class BaseOnMaster {
+	public static void main(String[] args) {
+		System.err.println("base on master");
+	}
+}
